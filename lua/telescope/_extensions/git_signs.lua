ff41f3d357b74a648b8741b230b63aeca5d73684
Daniel Eklund <doeklund@gmail.com>
2024-05-25 22:20:15 -0400

    added an export for actions so i can override the action_mapping

--- lua/telescope/_extensions/git_signs.lua
+++ lua/telescope/_extensions/git_signs.lua
@@ -1,7 +1,9 @@
 local pickers = require("pickers")
+local actions = require("actions")
 
 return require("telescope").register_extension({
     exports = {
         git_signs = pickers.git_signs,
+        actions = actions,
     },
 })
